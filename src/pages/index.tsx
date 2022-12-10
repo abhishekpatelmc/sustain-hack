b38993e74124f2f017d4f4d3ce4e0653ac5c2d88
Abhishek patel <mcabhishekpatel2017@gmail.com>
2022-12-10 16:21:48 -0500

    minor change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,7 +47,7 @@ const Home = () => {
               className="rounded-lg border-2 bg-slate-700 py-2 px-4 text-white  hover:border-slate-700 hover:bg-white hover:text-slate-700 "
               href="/posts"
             >
-              View Post
+              View Posts
             </Link>
 
             <Link
